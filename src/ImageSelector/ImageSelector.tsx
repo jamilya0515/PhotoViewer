33b81332335305b58514b0d0fb6ff932dc98ae06
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-02-11 14:20:00 +0000

    Image Selector added and is functioning

--- src/ImageSelector/ImageSelector.tsx
+++ src/ImageSelector/ImageSelector.tsx
@@ -1,7 +1,46 @@
-import React from "react"; 
+import React, { useState } from "react"
+import { PhotoViewer } from "../PhotoViewer/PhotoViewer"
+import './ImageSelector.css'
 
 
 export function ImageSelector() {
+    const brokenImages = [
+        1, 24, 32, 36, 44, 47
+      ];
+      function getImageUrls() {
+          const urls = [];
+          for (let i = 0; i < 50; i++) {
+              if (!brokenImages.includes(i)) {
+                  const imageNumberString = i.toString().padStart(2, '0');
+                  urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`)
+              }
+          }
+          return urls;
+      }
+    
+      const imageUrls = getImageUrls();
 
-    return <div>Hello</div>
+      const imageList = imageUrls.map(url => <button key={url} onClick={() => changeImage(url)}><PhotoViewer src={url}/></button>);
+
+    const [selectImage, setSelectImage] = useState('https://picsum.photos/200/300?random=2');
+
+    const changeImage = (url) => {
+        setSelectImage(url);
+        };
+    return (
+        <div>
+            {selectImage && (
+            <div className="selected-image">
+                <img src={selectImage} alt="Selected" />
+            </div>
+             )}
+
+        <h2>Select your photo</h2>
+            <div>
+                <ul className='pictures-container'>
+                    {imageList}
+                </ul>
+            </div>
+      </div>
+    )          
 }
